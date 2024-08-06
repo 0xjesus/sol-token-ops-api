aa351ad0aba68b66297481b4057e749679dd7d9d
0xjesus <pdlgmcn@protonmail.com>
2024-08-06 12:20:13 -0600

    ✨ (app.js): add support for default route using defaultRouter to handle requests to the root path ("/")

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import primate from '@thewebchimp/primate';
 import { router as solana } from './routes/solana.js';
+import {router as defaultRouter} from './routes/default.js';
 primate.setup();
 await primate.start();
 primate.app.use('/solana', solana);
+primate.app.use('/', defaultRouter);
